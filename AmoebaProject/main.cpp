be9e8fa86ffd2bd750e579a7346e07ef4d668592
Unknown <csacsa0319@gmail.com>
2017-05-13 13:09:56 +0200

    Cleaning main.cpp

--- AmoebaProject/main.cpp
+++ AmoebaProject/main.cpp
@@ -1,16 +1,7 @@
-#include "graphics.hpp"
-#include "widgets.hpp"
-#include <vector>
-#include <iostream>
-#include <string>
-#include <strstream>
-#include <sstream>
-#include <fstream>
-#include "field_button.hpp"
+
 #include "game_window.hpp"
 
 using namespace std;
-using namespace genv;
 
 
 
